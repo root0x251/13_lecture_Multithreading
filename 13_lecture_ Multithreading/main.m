1d0c8c5787e300a83feaebce95778db47a4aaeda
Slava <rtootik@gmail.com>
2017-03-09 17:40:46 +0300

    Initial Commit

---
+++ 13_lecture_ Multithreading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  13_lecture_ Multithreading
+//
+//  Created by Slava on 09.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
